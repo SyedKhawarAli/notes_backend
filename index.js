4479691ae2ab1f5787b3af4ba2693d65435291eb
Khawar Ali <khawarali5@gmail.com>
2023-08-03 18:44:36 +0300

    notes_backend

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+    console.log("Method: ", request.method)
+    console.log("Path: ", request.path)
+    console.log("Body: ", request.body)
+    console.log('---')
+    next()
+}
+
+const unkwonEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+const generateId = () => {
+    const maxid = notes.length > 0
+        ? Math.floor(Math.random() * 10000)
+        : 0
+    return maxid
+}
+
+app.post('/api/notes', (request, response) => {
+    
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        id: generateId(),
+        content: body.content,
+        important: body.important || false,
+    }
+    
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    let id = Number(request.params.id)
+    let note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    let id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.use(unkwonEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
