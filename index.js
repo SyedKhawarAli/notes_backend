c35d0fbec033f037330e0edaa98dee1b4c629bed
Khawar Ali <khawarali5@gmail.com>
2023-08-24 18:50:48 +0300

    uibuild

--- index.js
+++ index.js
@@ -25,33 +25,8 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(requestLogger)
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
-const generateId = () => {
-    const maxid = notes.length > 0
-        ? Math.floor(Math.random() * 10000)
-        : 0
-    return maxid
-}
-
 app.post('/api/notes', (request, response) => {
-    
+
     const body = request.body
 
     if (body.content === undefined) {
@@ -74,10 +49,31 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -89,6 +85,19 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.use(unkwonEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
